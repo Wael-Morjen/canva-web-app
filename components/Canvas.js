4998e16692669c0513be5a76941f78b7ccc80b7b
Wael-Morjen <waelmorjen@gmail.com>
2023-12-13 19:54:05 +0100

    Canvas Component init

---
+++ components/Canvas.js
@@ -0,0 +1,13 @@
+'use client';   
+
+import { useRef, useEffect } from 'react';
+
+const Canvas = ({ currentTool, addImage }) => {
+    return (
+        <div>
+            this is canvas
+        </div>
+    );
+}
+
+export default Canvas;
