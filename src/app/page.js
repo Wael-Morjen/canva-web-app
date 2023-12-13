420cc384517ccf23fb26c4cb61ba922deae633f2
Wael-Morjen <waelmorjen@gmail.com>
2023-12-13 19:54:26 +0100

    Canvas component integration

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Canvas from "../../components/Canvas";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      Hello Canva
+      <Canvas />
     </div>
   )
 }
