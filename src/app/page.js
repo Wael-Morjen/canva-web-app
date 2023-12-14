4af744f8f6e81420122e29923128f0320f4b0930
Wael-Morjen <waelmorjen@gmail.com>
2023-12-14 16:54:47 +0100

    add Tools component to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
+'use client';
+
+import Tools from "@/components/Tools";
 import Canvas from "../components/Canvas";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Canvas />
+      <Tools />
     </div>
   )
 }
