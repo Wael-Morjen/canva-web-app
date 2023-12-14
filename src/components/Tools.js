1e6ae29e467936c1737cc3629c36ecf647497ff3
Wael-Morjen <waelmorjen@gmail.com>
2023-12-14 16:54:21 +0100

    Tools component creation

---
+++ src/components/Tools.js
@@ -0,0 +1,12 @@
+import { FaPencilAlt, FaEraser, FaPalette, FaSave, FaCamera } from 'react-icons/fa';
+import { useState, useRef } from 'react';
+
+const Tools = () => {
+    return ( 
+        <div>
+            Tools section
+        </div>
+     );
+}
+ 
+export default Tools;
