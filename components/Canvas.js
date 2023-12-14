51b5541d120b48444d0c78f0807d33bfd9d8b0fe
Wael-Morjen <waelmorjen@gmail.com>
2023-12-14 15:29:47 +0100

    startDwaing function impl

--- components/Canvas.js
+++ components/Canvas.js
@@ -9,21 +9,33 @@ const Canvas = ({ currentTool, addImage }) => {
     
     // useEffect hook to set up the canvas and its context when the component mounts
     useEffect(() => {
-      const canvas = canvasRef.current;
+        const canvas = canvasRef.current;
       
-      // Set the width and height of the canvas to match the window dimensions
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
+        // Set the width and height of the canvas to match the window dimensions
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
 
-      const context = canvas.getContext('2d');
+        const context = canvas.getContext('2d');
       
-      // Set the line cap style to 'round' and the line width to 5 pixels
-      context.lineCap = 'round';
-      context.lineWidth = 5;
+        // Set the line cap style to 'round' and the line width to 5 pixels
+        context.lineCap = 'round';
+        context.lineWidth = 5;
       
-      // Save the context reference for future use
-      contextRef.current = context;
+        // Save the context reference for future use
+        contextRef.current = context;
     }, []);
+
+
+    // Function to handle the start of drawing
+    const startDrawing = ({ nativeEvent }) => {
+        const { offsetX, offsetY } = nativeEvent;
+
+        // Begin a new path and move to the starting point
+        contextRef.current.beginPath();
+        contextRef.current.moveTo(offsetX, offsetY);
+
+        isDrawing.current = true;
+    };    
   
     return (
         <div>
