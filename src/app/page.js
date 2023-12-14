105b0def1aefd03427f3aa174fd3ea6485bf067b
Wael-Morjen <waelmorjen@gmail.com>
2023-12-14 17:16:57 +0100

    selectPencil function impl

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 'use client';
 
 import Tools from "@/components/Tools";
-import Canvas from "../components/Canvas";
+import Canvas from "@/components/Canvas";
 
 export default function Home() {
+  const [currentTool, setCurrentTool] = useState('pencil');
+
+  // Function to set the current tool to 'pencil' when the pencil button is clicked
+  const selectPencil = () => {
+    setCurrentTool('pencil');
+  };
+
+  
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Canvas />
