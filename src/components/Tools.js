8a7f9a83b4575e487aaca6cd5c862973fc6ecfa0
Wael-Morjen <waelmorjen@gmail.com>
2023-12-15 17:16:48 +0100

    Refactor the component with ToolButton func

--- src/components/Tools.js
+++ src/components/Tools.js
@@ -1,20 +1,11 @@
 import { FaPencilAlt, FaEraser, FaSave, FaCamera, FaFile } from 'react-icons/fa';
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 
 const Tools = ({ selectPencil, selectEraser, selectColor, saveDrawing, addImage, isDrawing }) => {
   const [isCameraOpen, setIsCameraOpen] = useState(false);
-  const [capturedImage, setCapturedImage] = useState(null);
   const inputRef = useRef(null);
 
-  const openCamera = () => {
-    setIsCameraOpen(true);
-  };
-
-  const closeCamera = () => {
-    setIsCameraOpen(false);
-  };
-
-  const takeCameraShot = async () => {
+  const openCamera = async () => {
     try {
       const mediaStream = await navigator.mediaDevices.getUserMedia({ video: true });
 
@@ -32,7 +23,7 @@ const Tools = ({ selectPencil, selectEraser, selectColor, saveDrawing, addImage,
         context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
         const imageData = canvas.toDataURL('image/png');
-        setCapturedImage(imageData);
+        addImage(imageData);
 
         mediaStream.getTracks().forEach((track) => track.stop());
         document.body.removeChild(video);
@@ -70,48 +61,16 @@ const Tools = ({ selectPencil, selectEraser, selectColor, saveDrawing, addImage,
     return tooltips[tool.toLowerCase()] || '';
   };
 
-  useEffect(() => {
-    // If a photo is captured, add it to the canvas
-    if (capturedImage) {
-      addImage(capturedImage);
-      setCapturedImage(null); // Reset capturedImage state
-    }
-  }, [capturedImage, addImage]);
-
   return (
     <div className={`fixed top-4 left-1/2 transform -translate-x-1/2 sm:flex sm:justify-center sm:items-center ${isDrawing ? 'hidden' : ''}`}>
       <div className="bg-white p-3 rounded-lg border border-gray-300 flex items-center flex-wrap gap-4">
-        <button
-          onClick={selectPencil}
-          className="p-3 rounded-full hover:bg-gray-200 relative transition duration-300 flex flex-col items-center"
-        >
-          <div className="flex items-center flex-col">
-            <FaPencilAlt size={24} />
-            <span className="tooltip">{getTooltipText('pencil')}</span>
-          </div>
-        </button>
-        <button
-          onClick={selectEraser}
-          className="p-3 rounded-full hover:bg-gray-200 relative transition duration-300 flex flex-col items-center"
-        >
-          <div className="flex items-center flex-col">
-            <FaEraser size={24} />
-            <span className="tooltip">{getTooltipText('eraser')}</span>
-          </div>
-        </button>
-        <button
-          onClick={saveDrawing}
-          className="p-3 rounded-full hover:bg-gray-200 relative transition duration-300 flex flex-col items-center"
-        >
-          <div className="flex items-center flex-col">
-            <FaSave size={24} />
-            <span className="tooltip">{getTooltipText('save')}</span>
-          </div>
-        </button>
+        <ToolButton onClick={selectPencil} icon={<FaPencilAlt size={24} />} tooltip={getTooltipText('pencil')} />
+        <ToolButton onClick={selectEraser} icon={<FaEraser size={24} />} tooltip={getTooltipText('eraser')} />
+        <ToolButton onClick={saveDrawing} icon={<FaSave size={24} />} tooltip={getTooltipText('save')} />
 
         {/* Camera button will only be visible on small screens (mobile devices) */}
         <button
-          onClick={isCameraOpen ? closeCamera : openCamera}
+          onClick={openCamera}
           className={`p-3 rounded-full hover:bg-gray-200 relative transition duration-300 sm:hidden flex flex-col items-center ${isCameraOpen ? 'text-blue-500' : ''}`}
         >
           <div className="flex items-center flex-col">
@@ -121,8 +80,8 @@ const Tools = ({ selectPencil, selectEraser, selectColor, saveDrawing, addImage,
         </button>
 
         <button
-          onClick={() => inputRef.current.click()} // Trigger file input when button is clicked
           className="cursor-pointer p-3 rounded-full hover:bg-gray-200 relative transition duration-300 flex flex-col items-center"
+          onClick={() => inputRef.current.click()}
         >
           <div className="flex items-center flex-col">
             <label htmlFor="file-upload" className="cursor-pointer">
@@ -139,18 +98,34 @@ const Tools = ({ selectPencil, selectEraser, selectColor, saveDrawing, addImage,
             <span className="tooltip">{getTooltipText('file')}</span>
           </div>
         </button>
+
         <div className="flex items-center ml-4">
           {colors.map((color) => (
             <button
               key={color}
               onClick={() => selectColor(color)}
               style={{ backgroundColor: color }}
-              className="w-8 h-8 mx-1 border border-gray-600 rounded-full hover:bg-gray-200"
+              className="w-8 h-8 mx-1 border border-gray-600 rounded-full hover:bg-gray-200 transition duration-300"
             />
           ))}
         </div>
       </div>
     </div>
+  );
+};
+
+// A reusable button with transitions
+const ToolButton = ({ onClick, icon, tooltip }) => {
+  return (
+    <button
+      onClick={onClick}
+      className="p-3 rounded-full hover:bg-gray-200 relative transition duration-300 flex flex-col items-center"
+    >
+      <div className="flex items-center flex-col">
+        {icon}
+        <span className="tooltip">{tooltip}</span>
+      </div>
+    </button>
   );
 };
 
